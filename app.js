58b15d064dcd37ebad0498266e0e7aef0ba37da2
unknown <sizwesinda106@gmail.com>
2022-07-22 11:17:33 +0200

    tracker js

---
+++ app.js
@@ -0,0 +1,4 @@
+var submitBtn = document.getElementById('submit');
+var gamertagInput = document.getElementById('gamertag');
+var platformInput = document.getElementById('platform');
+var result = document.querySelector('.result');
